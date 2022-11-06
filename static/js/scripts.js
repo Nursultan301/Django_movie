35401c19caecb3b4d6b93e9e9a96030432c36c34
Nursultan301 <abykeevnursultan14@gmail.com>
2022-11-06 19:06:33 +0600

    Created a folder js and req.txt

---
+++ static/js/scripts.js
@@ -0,0 +1,13 @@
+// Add star rating
+const rating = document.querySelector('form[name=rating]');
+
+rating.addEventListener("change", function (e) {
+    // Получаем данные из формы
+    let data = new FormData(this);
+    fetch(`${this.action}`, {
+        method: 'POST',
+        body: data
+    })
+        .then(response => alert("Рейтинг установлен"))
+        .catch(error => alert("Ошибка"))
+});
