9df2ba2481d116bc0cf0af44c97daa2c517c5d45
Nursultan301 <abykeevnursultan14@gmail.com>
2022-11-07 19:44:51 +0600

    Add local

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -6,11 +7,14 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('pages/', include('django.contrib.flatpages.urls')),
-    path('contact/', include('contact.urls')),
-    path('', include('movie.urls'))
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
+urlpatterns += i18n_patterns(
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('contact/', include("contact.urls")),
+    path("", include("movie.urls")),
+)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
