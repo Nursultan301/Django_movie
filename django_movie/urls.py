e88014d779624934aa036f35fd40a6487d0d19a2
Nursultan301 <abykeevnursultan14@gmail.com>
2022-11-12 10:07:26 +0600

    updated urls and base.html

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -4,13 +4,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
-]
-
-urlpatterns += i18n_patterns(
     path('pages/', include('django.contrib.flatpages.urls')),
     path('contact/', include("contact.urls")),
     path("", include("movie.urls")),
